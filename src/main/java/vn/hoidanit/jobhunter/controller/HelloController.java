10b11a5cafe4968152824176b57500c450a7cbec
Phạm Tuấn Vương <phamtuanvuong.dev@gmail.com>
2025-02-05 15:40:55 +0700

    initialize

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -8,6 +8,11 @@ public class HelloController {
 
     @GetMapping("/")
     public String getHelloWorld() {
-        return "Hello World (Hỏi Dân IT & Eric)";
+        return "Hello World (Hỏi Dân IT )";
+    }
+
+    @GetMapping("/123123")
+    public String getHelloWorld123String() {
+        return "Hello World (Hỏi Dân IT)";
     }
 }
